3f67db1dbd553192296b686366d8b813d64367ea
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-22 23:27:29 +0900

    feat: Add files

--- 30_스택 2/05_3015.swift
+++ 30_스택 2/05_3015.swift
@@ -1,1 +1,25 @@
+let n = Int(readLine() ?? "") ?? 0
+var heights = (0..<n).map { _ in
+    Int(readLine() ?? "") ?? 0
+}
 
+var stack: [(height: Int, cnt: Int)] = []
+var count = 0
+
+for i in 0..<n {
+    let curHeight = heights[i]
+    var cnt = 1
+    
+    while let last = stack.last, last.height <= curHeight {
+        let top = last
+        
+        cnt += top.height == curHeight ? top.cnt : 0
+        count += top.cnt
+        stack.removeLast()
+    }
+    
+    if !stack.isEmpty { count += 1 }
+    stack.append((curHeight, cnt))
+}
+
+print(count)
