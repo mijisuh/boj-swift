1b665227b46d2d93e289cfbe12800f67cc763e44
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-25 04:10:49 +0900

    feat: Add files

--- 31_그래프와 순회/10_1697.swift
+++ 31_그래프와 순회/10_1697.swift
@@ -1,1 +1,23 @@
+let nk = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (n, k) = (nk[0], nk[1])
 
+var visited = [Int](repeating: 0, count: 100_001)
+var queue = [n]
+var index = 0
+
+if n == k {
+    print(0)
+} else if n > k { // -1 하는 방법만 가능
+    print(n - k)
+} else {
+    while queue.count > index {
+        let pos = queue[index]
+        for i in [pos - 1, pos + 1, pos * 2] where (0...100_000) ~= i && visited[i] == 0 {
+            visited[i] = visited[pos] + 1
+            queue.append(i)
+        }
+        index += 1
+    }
+    
+    print(visited[k])
+}
--- 31_그래프와 순회/11_7562.swift
+++ 31_그래프와 순회/11_7562.swift
@@ -1,1 +1,31 @@
+let dy = [-2, -1, -2, -1, 2, 1, 2, 1]
+let dx = [-1, -2, 1, 2, -1, -2, 1, 2]
 
+let t = Int(readLine() ?? "") ?? 0
+for _ in 0..<t {
+    let i = Int(readLine() ?? "") ?? 0
+    let start = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+    let dest = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+
+    var visited = [[Int]](repeating: [Int](repeating: 0, count: i), count: i)
+    var queue = [start]
+    var index = 0
+    visited[start[1]][start[0]] = 1
+    
+    while queue.count > index, visited[dest[1]][dest[0]] == 0 { // dest 도착 여부 확인하면 시간 단축(168ms -> 96ms)
+        let (x, y) = (queue[index][0], queue[index][1])
+
+        for idx in 0..<8 {
+            let (nx, ny) = (x + dx[idx], y + dy[idx])
+
+            if (0..<i) ~= nx, (0..<i) ~= ny, visited[ny][nx] == 0 {
+                visited[ny][nx] = visited[y][x] + 1
+                queue.append([nx, ny])
+            }
+        }
+        
+        index += 1
+    }
+
+    print(visited[dest[1]][dest[0]] - 1)
+}
