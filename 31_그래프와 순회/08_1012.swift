f9ff4c4dfb238347d3d3807df0d91f65da5bc3ae
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-25 01:12:08 +0900

    feat: Add files

--- 31_그래프와 순회/08_1012.swift
+++ 31_그래프와 순회/08_1012.swift
@@ -1,1 +1,46 @@
+var dy = [1, -1, 0, 0]
+var dx = [0, 0, 1, -1]
 
+func isValid(_ y: Int, _ x: Int, _ n: Int, _ m: Int) -> Bool {
+    return (0..<n) ~= y && (0..<m) ~= x
+}
+
+func dfs(_ y: Int, _ x: Int, _ map: inout [[Int]], _ n: Int, _ m: Int) {
+    map[y][x] = 0
+
+    for i in 0..<4 {
+        let ny = y + dy[i]
+        let nx = x + dx[i]
+
+        if isValid(ny, nx, n, m), map[ny][nx] == 1 {
+            dfs(ny, nx, &map, n, m)
+        }
+    }
+}
+
+let _ = readLine()
+
+while let input = readLine() {
+    let MNK = input.split { $0 == " " }.map { Int($0) ?? 0 }
+    let (M, N, K) = (MNK[0], MNK[1], MNK[2])
+
+    var map = [[Int]](repeating: [Int](repeating: 0, count: M), count: N)
+    for _ in 0..<K {
+        let XY = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+        let (X, Y) = (XY[0], XY[1])
+        map[Y][X] = 1
+    }
+
+    var count = 0
+    
+    for y in 0..<N {
+        for x in 0..<M {
+            if map[y][x] == 1 {
+                count += 1
+                dfs(y, x, &map, N, M)
+            }
+        }
+    }
+
+    print(count)
+}
--- 31_그래프와 순회/09_2178.swift
+++ 31_그래프와 순회/09_2178.swift
@@ -1,1 +1,35 @@
+// 최소의 이동이기 때문에 BFS
+let nm = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (n, m) = (nm[0], nm[1])
+var map: [[Int]] = (0..<n).map { _ in
+    (readLine() ?? "").map { Int(String($0)) ?? 0 }
+}
 
+let dy = [1, -1, 0, 0]
+let dx = [0, 0, 1, -1]
+
+func isValid(_ y: Int, _ x: Int) -> Bool {
+    return (0..<n) ~= y && (0..<m) ~= x
+}
+
+func bfs(_ y: Int, _ x: Int, _ count: Int) {
+    var queue: [(y: Int, x: Int)] = [(y, x)]
+    var index = 0
+    
+    while queue.count > index {
+        for i in 0..<4 {
+            let (y, x) = (queue[index].y, queue[index].x)
+            let (ny, nx) = (y + dy[i], x + dx[i])
+            
+            if isValid(ny, nx), map[ny][nx] == 1 {
+                map[ny][nx] = map[y][x] + 1 // nx, ny로 이동하기 전의 위치에 있는 값에 1을 더해서 삽입(누적)
+                // 이렇게 하면 이미 방문한 좌표는 1이 아니기 때문에 재방문하지 않음
+                queue.append((ny, nx))
+            }
+        }
+        index += 1
+    }
+}
+
+bfs(0, 0, 0)
+print(map[n - 1][m - 1])
