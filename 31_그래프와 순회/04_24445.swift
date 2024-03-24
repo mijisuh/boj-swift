738887c8bcb0d0b204a8ea42f3f662e1617f20d0
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-24 19:18:48 +0900

    feat: Add files

--- 31_그래프와 순회/04_24445.swift
+++ 31_그래프와 순회/04_24445.swift
@@ -1,1 +1,28 @@
+let NMR = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (N, M, R) = (NMR[0], NMR[1], NMR[2])
+var edges: [Int: [Int]] = [:]
+for _ in 0..<M {
+    let UV = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+    let (U, V) = (UV[0], UV[1])
+    edges[U, default: []].append(V)
+    edges[V, default: []].append(U) // 양방향
+}
 
+var visited = [Int](repeating: 0, count: N + 1) // 해당 인덱스(정점)에 방문하는 순서 저장
+var order = 1
+func bfs(_ node: Int) {
+    visited[node] = order
+    var queue = [node]
+    var index = 0 // queue의 인덱스
+    while queue.count > index {
+        for vertex in edges[queue[index], default: []].sorted(by: >) where visited[vertex] == 0 {
+            order += 1
+            visited[vertex] = order
+            queue.append(vertex)
+        }
+        index += 1
+    }
+}
+
+bfs(R)
+print(visited[1...].map { String($0) }.joined(separator: "\n"))
--- 31_그래프와 순회/05_2606.swift
+++ 31_그래프와 순회/05_2606.swift
@@ -1,1 +1,22 @@
+let N = Int(readLine() ?? "") ?? 0
+let M = Int(readLine() ?? "") ?? 0
+var edges: [Int: [Int]] = [:]
+for _ in 0..<M {
+    let UV = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+    let (U, V) = (UV[0], UV[1])
+    edges[U, default: []].append(V)
+    edges[V, default: []].append(U) // 양방향
+}
 
+var visited = [Bool](repeating: false, count: N + 1) // 해당 인덱스(정점)에 방문 여부 저장
+var count = 0
+func dfs(_ node: Int) {
+    visited[node] = true
+    for next in edges[node, default: []].sorted() where !visited[next] { // 인접한 정점 탐색
+        count += 1
+        dfs(next)
+    }
+}
+
+dfs(1)
+print(count)
