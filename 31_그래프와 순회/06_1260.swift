fadb1f8774901e4be41071702574d134ff48a8fb
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-24 22:49:58 +0900

    feat: Add files

--- 31_그래프와 순회/06_1260.swift
+++ 31_그래프와 순회/06_1260.swift
@@ -1,1 +1,49 @@
+let NMV = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (N, M, V) = (NMV[0], NMV[1], NMV[2])
+var edges = [[Int]](repeating: [Int](repeating: 0, count: 0), count: N + 1)
+for _ in 0..<M {
+    let UV = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+    let (U, V) = (UV[0], UV[1])
+    edges[U].append(V)
+    edges[V].append(U) // 양방향
+}
 
+var result = ""
+var visited = [Bool](repeating: false, count: N + 1) // 해당 인덱스(정점)에 방문 여부 저장
+var order = 1
+
+func dfs(_ node: Int) {
+    visited[node] = true
+    result += "\(node) "
+    for next in edges[node].sorted() where !visited[next] { // 인접한 정점 중 방문하지 않은 정점 탐색
+        order += 1
+        dfs(next)
+    }
+}
+
+dfs(V)
+result += "\n"
+
+visited = [Bool](repeating: false, count: N + 1)
+order = 1
+
+func bfs(_ node: Int) {
+    var queue = [node]
+    var index = 0
+
+    visited[node] = true
+    result += "\(node) "
+    while queue.count > index {
+        for next in edges[queue[index]].sorted() where !visited[next] {
+            queue.append(next)
+            order += 1
+            visited[next] = true
+            result += "\(next) "
+        }
+        index += 1
+    }
+}
+
+bfs(V)
+
+print(result)
--- 31_그래프와 순회/07_2667.swift
+++ 31_그래프와 순회/07_2667.swift
@@ -1,1 +1,41 @@
+let N = Int(readLine() ?? "") ?? 0
+var map = [[Int]](repeating: [Int](repeating: 0, count: N), count: N)
+for i in 0..<N {
+    map[i] = (readLine() ?? "").map { Int(String($0)) ?? 0 }
+}
 
+let dx = [0, 0, -1, 1] // 상, 하, 좌, 우
+let dy = [1, -1, 0, 0]
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: N), count: N)
+
+func isValid(_ y: Int, _ x: Int) -> Bool {
+    return (0..<N) ~= y && (0..<N) ~= x
+}
+
+func dfs(_ y: Int, _ x: Int) -> Int {
+    visited[y][x] = true
+    var count = 1
+
+    for i in 0..<4 {
+        if isValid(y + dy[i], x + dx[i]),
+            !visited[y + dy[i]][x + dx[i]],
+            map[y + dy[i]][x + dx[i]] == 1 {
+            count += dfs(y + dy[i], x + dx[i])
+        }
+    }
+
+    return count
+}
+
+var counts: [Int] = []
+
+for y in 0..<N {
+    for x in 0..<N {
+        if !visited[y][x], map[y][x] == 1 {
+            counts.append(dfs(y, x))
+        }
+    }
+}
+
+print(counts.count)
+counts.sorted().forEach { print($0) }
