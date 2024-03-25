107bf55c04b641d4a1c8dc705ded51811b5fd1c9
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-25 17:39:02 +0900

    feat: Add files

--- 31_그래프와 순회/12_7576.swift
+++ 31_그래프와 순회/12_7576.swift
@@ -1,1 +1,36 @@
+let mn = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (m, n) = (mn[0], mn[1])
+var map = (0..<n).map { _ in
+    (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+}
 
+var queue: [(x: Int, y: Int)] = []
+for i in 0..<n {
+    for j in 0..<m {
+        if map[i][j] == 1 {
+            queue.append((j, i))
+        }
+    }
+}
+
+let dy = [1, -1, 0, 0]
+let dx = [0, 0, 1, -1]
+
+var index = 0
+
+while queue.count > index {
+    let (x, y) = queue[index]
+
+    for i in 0..<4 {
+        let (nx, ny) = (x + dx[i], y + dy[i])
+        if (0..<m) ~= nx, (0..<n) ~= ny, map[ny][nx] == 0, map[ny][nx] != -1 {
+            map[ny][nx] = map[y][x] + 1
+            queue.append((nx, ny))
+        }
+    }
+    index += 1
+}
+
+let flat = map.flatMap { $0 }
+
+print(flat.contains(0) ? -1 : flat.max()! - 1)
--- 31_그래프와 순회/13_7569.swift
+++ 31_그래프와 순회/13_7569.swift
@@ -1,1 +1,40 @@
+let mnh = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (m, n, h) = (mnh[0], mnh[1], mnh[2])
+var map: [[[Int]]] = (0..<h).map { _ in
+    (0..<n).map { _ in
+        (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+    }
+}
 
+var queue: [(x: Int, y: Int, z: Int)] = []
+for i in 0..<h {
+    for j in 0..<n {
+        for k in 0..<m {
+            if map[i][j][k] == 1 {
+                queue.append((k, j, i))
+            }
+        }
+    }
+}
+
+let dz = [1, -1, 0, 0, 0, 0]
+let dy = [0, 0, 1, -1, 0, 0]
+let dx = [0, 0, 0, 0, 1, -1]
+
+var index = 0
+
+while queue.count > index {
+    let (x, y, z) = queue[index]
+
+    for i in 0..<6 {
+        let (nx, ny, nz) = (x + dx[i], y + dy[i], z + dz[i])
+        if (0..<m) ~= nx, (0..<n) ~= ny, (0..<h) ~= nz, map[nz][ny][nx] == 0, map[nz][ny][nx] != -1 {
+            map[nz][ny][nx] = map[z][y][x] + 1
+            queue.append((nx, ny, nz))
+        }
+    }
+    index += 1
+}
+
+let flat = map.flatMap { $0 }.flatMap { $0 }
+print(flat.contains(0) ? -1 : flat.max()! - 1)
