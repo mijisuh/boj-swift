c94c87174e48c0861b05d1f65669422ccc69987a
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-29 20:39:00 +0900

    feat: Add files

--- 33_투 포인터/03_1806.swift
+++ 33_투 포인터/03_1806.swift
@@ -1,1 +1,18 @@
+let ns = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (n, s) = (ns[0], ns[1])
+let array = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
 
+var right = 0
+var psum = 0
+var answer = n + 1
+
+for left in 0..<n { // left를 포함한다고 가정
+    while psum < s && right < n { // 부분합 계산
+        psum += array[right]
+        right += 1
+    }
+    if psum >= s { answer = min(answer, right - left) }
+    psum -= array[left] // left 포함 X
+}
+
+print(answer == n + 1 ? 0 : answer)
--- 33_투 포인터/04_1644.swift
+++ 33_투 포인터/04_1644.swift
@@ -1,1 +1,43 @@
+func getPrimes(_ n: Int) -> [Int] { // 에라토스테네스의 체
+    if n < 2 { return [] }
+    
+    var isPrime = [Bool](repeating: true, count: n + 1)
+    isPrime[0] = false
+    isPrime[1] = false
 
+    var i = 2
+    while i * i <= n {
+        if isPrime[i] {
+            var multiple = i * i // 2, 3, ... 의 배수
+            while multiple <= n {
+                isPrime[multiple] = false // 배수이므로 소수가 아님
+                multiple += i
+            }
+        }
+
+        i += 1
+    }
+    
+    return isPrime.enumerated().filter { $0.element }.map { $0.offset } // 값이 1인 경우 그 인덱스
+}
+
+let n = Int(readLine() ?? "") ?? 0
+
+let primes = getPrimes(n)
+
+var right = 0
+var psum = 0
+var count = 0
+
+for left in 0..<primes.count {
+    while psum < n, right < primes.count {
+        psum += primes[right]
+        right += 1
+    }
+
+    if psum == n { count += 1 }
+
+    psum -= primes[left]
+}
+
+print(count)
