4155feac9b9581e87c7eba2aa3b2a588b5b356ea
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-03-31 21:08:35 +0900

    feat: Add files

--- 34_동적계획법과 최단거리 역추적/02_14002.swift
+++ 34_동적계획법과 최단거리 역추적/02_14002.swift
@@ -1,1 +1,25 @@
+let n = Int(readLine() ?? "") ?? 0
+let array = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
 
+var dp = [Int](repeating: 1, count: n)
+
+for i in 1..<n {
+    for j in 0..<i {
+        if array[j] < array[i] {
+            dp[i] = max(dp[i], dp[j] + 1)
+        }
+    }
+}
+
+var count = dp.max() ?? 0
+print(count)
+
+var result: [Int] = []
+// 인덱스 역추적
+for i in (0..<n).reversed() { // dp 역순으로
+    if dp[i] == count { // 내림차순으로
+        result.append(array[i]) // 해당하는 값 저장
+        count -= 1 // 중복없이
+    }
+}
+print(result.reversed().map { String($0) }.joined(separator: " "))
--- 34_동적계획법과 최단거리 역추적/03_14003.swift
+++ 34_동적계획법과 최단거리 역추적/03_14003.swift
@@ -1,1 +1,44 @@
+func lowerbound(_ array: [Int], _ target: Int) -> Int { // target보다 작거나 같은 값 중 가장 가까운 값의 인덱스
+    var left = 0, right = array.count - 1
+    var mid: Int { (left + right) / 2 }
 
+    while left <= right {
+        if array[mid] < target {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+
+    return left
+}
+
+let n = Int(readLine() ?? "") ?? 0
+let array = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+
+var lis = [array[0]] // 오름차순으로 정렬됨
+var index: [Int] = [0]
+
+for i in 1..<n {
+    if lis.last! < array[i] {
+        lis.append(array[i])
+        index.append(lis.count - 1)
+    } else {
+        let lowerbound = lowerbound(lis, array[i]) // 현재 값과 가장 가까이에 있는 작거나 같은 값을
+        lis[lowerbound] = array[i] // 현재 값으로 대체
+        index.append(lowerbound)
+    }
+}
+
+var count = index.max()! + 1 // lis.count
+print(count)
+
+var result: [Int] = []
+for i in (0..<n).reversed() {
+    if index[i] + 1 == count {
+        result.append(array[i])
+        count -= 1
+    }
+}
+
+print(result.reversed().map { String($0) }.joined(separator: " "))
