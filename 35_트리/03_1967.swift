9704f8964a9380b90a3534587b177bace2e0faba
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-04-04 22:33:56 +0900

    feat: Add files

--- 35_트리/03_1967.swift
+++ 35_트리/03_1967.swift
@@ -1,1 +1,33 @@
+typealias Node = (node: Int, weight: Int)
 
+func dfs(_ node: Int, _ length: Int) {
+    visited[node] = true
+
+    if endNode.weight < length {
+        endNode = Node(node, length)
+    }
+
+    for (next, weight) in graph[node] where !visited[next] {
+        dfs(next, length + weight)
+    }
+}
+
+let n = Int(readLine() ?? "") ?? 0
+var graph = [[Node]](repeating: [], count: n + 1)
+while let input = readLine() {
+    let uvw = input.split { $0 == " " }.map { Int($0) ?? 0 }
+    let (u, v, w) = (uvw[0], uvw[1], uvw[2])
+    graph[u].append(Node(v, w))
+    graph[v].append(Node(u, w))
+}
+
+var visited = [Bool](repeating: false, count: n + 1)
+var endNode = Node(0, 0)
+
+dfs(1, 0)
+
+visited = [Bool](repeating: false, count: n + 1)
+
+dfs(endNode.node, 0)
+
+print(endNode.weight)
--- 35_트리/04_1991.swift
+++ 35_트리/04_1991.swift
@@ -1,1 +1,36 @@
+typealias Child = (left: String, right: String)
 
+func preorder(_ node: String) { // node -> left -> right
+    if node == "." { return }
+    print(node, terminator: "")
+    preorder(tree[node]!.left)
+    preorder(tree[node]!.right)
+}
+
+func inorder(_ node: String) { // left -> node -> right
+    if node == "." { return }
+    inorder(tree[node]!.left)
+    print(node, terminator: "")
+    inorder(tree[node]!.right)
+}
+
+func postorder(_ node: String) { // left -> right -> node
+    if node == "." { return }
+    postorder(tree[node]!.left)
+    postorder(tree[node]!.right)
+    print(node, terminator: "")
+}
+
+let n = Int(readLine() ?? "") ?? 0
+var tree: [String: Child] = [:]
+while let input = readLine() {
+    let inputs = input.split { $0 == " " }.map { String($0) }
+    let (node, left, right) = (inputs[0], inputs[1], inputs[2])
+    tree[node] = Child(left, right)
+}
+
+preorder("A")
+print()
+inorder("A")
+print()
+postorder("A")
