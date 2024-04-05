f00139afedd1e65fdfec5738af3648c926e38c86
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-04-05 21:45:39 +0900

    feat: Add files

--- 35_트리/05_2263.swift
+++ 35_트리/05_2263.swift
@@ -1,1 +1,55 @@
+// 방법 1) Array.firstIndex(of:)
+func dfs(_ inStart: Int, _ inEnd: Int, _ postStart: Int, _ postEnd: Int) {
+    guard inStart <= inEnd, postStart <= postEnd else { return }
 
+    let root = postOrder[postEnd]
+    answer += "\(root) "
+
+    let rootIndex = inOrder.firstIndex(of: root)!
+
+    let leftCount = rootIndex - inStart - 1
+    let rightCount = inEnd - rootIndex
+
+    dfs(inStart, rootIndex - 1, postStart, postStart + leftCount)
+    dfs(rootIndex + 1, inEnd, postEnd - rightCount, postEnd - 1)
+}
+
+let n = Int(readLine() ?? "") ?? 0
+let inOrder = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let postOrder = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+
+var answer = ""
+
+dfs(0, n - 1, 0, n - 1)
+print(answer)
+
+// 방법 2) index 테이블 생성
+// 시간 단축: 2516 ms(방법 1) -> 116 ms
+func dfs(_ inStart: Int, _ inEnd: Int, _ postStart: Int, _ postEnd: Int) {
+    guard inStart <= inEnd, postStart <= postEnd else { return }
+
+    let root = postOrder[postEnd]
+    answer += "\(root) "
+
+    let rootIndex = inOrderDict[root]!
+
+    let leftCount = rootIndex - inStart - 1
+    let rightCount = inEnd - rootIndex
+
+    dfs(inStart, rootIndex - 1, postStart, postStart + leftCount)
+    dfs(rootIndex + 1, inEnd, postEnd - rightCount, postEnd - 1)
+}
+
+let n = Int(readLine() ?? "") ?? 0
+let inOrder = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let postOrder = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+
+var inOrderDict: [Int: Int] = [:]
+inOrder.enumerated().forEach { index, value in
+    inOrderDict[value] = index
+}
+
+var answer = ""
+
+dfs(0, n - 1, 0, n - 1)
+print(answer)
--- 35_트리/06_5639.swift
+++ 35_트리/06_5639.swift
@@ -1,1 +1,63 @@
+// 방법 1) 완전 탐색
+func findHigherIndex(_ start: Int, _ end: Int, _ compare: Int) -> Int {
+    var higherIndex: Int = end
+    for i in start..<end {
+        if compare < preOrder[i] {
+            higherIndex = i
+            break
+        }
+    }
+    return higherIndex
+}
 
+func dfs(_ start: Int, _ end: Int) {
+    guard start < end else { return }
+
+    let root = preOrder[start]
+    let higherIndex: Int = findHigherIndex(start + 1, end, root)
+
+    dfs(start + 1, higherIndex)
+    dfs(higherIndex, end)
+    print(root)
+}
+
+var preOrder: [Int] = []
+while let input = Int(readLine() ?? "") {
+    preOrder.append(input)
+}
+
+dfs(0, preOrder.count)
+
+// 방법 2) upperbound
+// 시간 단축: 60 ms(방법 1) -> 28 ms
+func upperbound(_ start: Int, _ end: Int, _ target: Int) -> Int {
+    var s = start
+    var e = end
+    var mid: Int { (s + e) / 2 }
+    while s < e {
+        if target < preOrder[mid] {
+            e = mid
+        } else {
+            s = mid + 1
+        }
+    }
+    return s
+}
+
+func dfs(_ start: Int, _ end: Int) {
+    guard start < end else { return }
+    
+    let root = preOrder[start]
+    let upperbound = upperbound(start + 1, end, root)
+    
+    dfs(start + 1, upperbound)
+    dfs(upperbound, end)
+    print(root)
+}
+
+var preOrder: [Int] = []
+while let input = Int(readLine() ?? "") {
+    preOrder.append(input)
+}
+
+dfs(0, preOrder.count)
