87d742d96c4ccdacaa8f75b7b5c04cae1a75e371
suhmiji <57468832+suhmiji@users.noreply.github.com>
2024-04-13 18:27:26 +0900

    feat: Add files

--- 37_최소 신장 트리/05_6497.swift
+++ 37_최소 신장 트리/05_6497.swift
@@ -1,1 +1,217 @@
+// 빠른 입출력 사용 안하면 시간 초과
+// 방법 1: 전체 비용 - 최소 비용
+import Foundation
 
+typealias Edge = (x: Int, y: Int, z: Int)
+
+func find(_ x: Int, _ parent: inout [Int]) -> Int {
+    if parent[x] == x { return x }
+    parent[x] = find(parent[x], &parent)
+    return parent[x]
+}
+
+func merge(_ x: Int, _ y: Int, _ parent: inout [Int]) {
+    let parentX = find(x, &parent)
+    let parentY = find(y, &parent)
+
+    if parentX < parentY {
+        parent[parentY] = parentX
+    } else if parentY < parentX {
+        parent[parentX] = parentY
+    }
+}
+
+func isCycle(_ x: Int, _ y: Int, _ parent: inout [Int]) -> Bool {
+    return find(x, &parent) == find(y, &parent)
+}
+
+func kruskal(_ m: Int, _ edges: inout [Edge]) -> Int {
+    var parent = [Int](0...m)
+    var minCost = 0
+
+    edges.sort { $0.z < $1.z }
+
+    for (x, y, z) in edges where !isCycle(x, y, &parent) {
+        merge(x, y, &parent)
+        minCost += z
+    }
+
+    return minCost
+}
+
+let file = FileIO()
+
+while true {
+    let (m, n) = (file.readInt(), file.readInt())
+
+    if m == 0, n == 0 { break }
+
+    var totalCost = 0
+    var edges: [Edge] = []
+    for _ in 0..<n {
+        let (x, y, z) = (file.readInt(), file.readInt(), file.readInt())
+        edges.append((x, y, z))
+        totalCost += z
+    }
+
+    print(totalCost - kruskal(m, &edges)) // 전체 비용 - 최소 비용
+}
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
+
+// 방법 2: 최소비용이 아닌 경로의 비용
+import Foundation
+
+typealias Edge = (x: Int, y: Int, z: Int)
+
+func find(_ x: Int, _ parent: inout [Int]) -> Int {
+    if parent[x] == x { return x }
+    parent[x] = find(parent[x], &parent)
+    return parent[x]
+}
+
+func merge(_ x: Int, _ y: Int, _ parent: inout [Int]) {
+    let parentX = find(x, &parent)
+    let parentY = find(y, &parent)
+
+    if parentX < parentY {
+        parent[parentY] = parentX
+    } else if parentY < parentX {
+        parent[parentX] = parentY
+    }
+}
+
+func isCycle(_ x: Int, _ y: Int, _ parent: inout [Int]) -> Bool {
+    return find(x, &parent) == find(y, &parent)
+}
+
+func kruskal(_ m: Int, _ edges: inout [Edge]) -> Int {
+    var parent = [Int](0...m)
+    var answer = 0
+
+    edges.sort { $0.z < $1.z }
+
+    for (x, y, z) in edges {
+        if isCycle(x, y, &parent) {
+            answer += z // 최소비용의 경로로 가지 않는 경우
+        } else {
+            merge(x, y, &parent)
+        }
+    }
+
+    return answer
+}
+
+let file = FileIO()
+
+while true {
+    let (m, n) = (file.readInt(), file.readInt())
+
+    if m == 0, n == 0 { break }
+
+    var totalCost = 0
+    var edges: [Edge] = []
+    for _ in 0..<n {
+        let (x, y, z) = (file.readInt(), file.readInt(), file.readInt())
+        edges.append((x, y, z))
+        totalCost += z
+    }
+
+    print(kruskal(m, &edges))
+}
+
+// 라이노님의 FileIO
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+    
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+    
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+        
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+        
+        return sum * (isPositive ? 1:-1)
+    }
+    
+    @inline(__always) func readString() -> String {
+        var str = ""
+        var now = read()
+        
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        
+        while now != 10
+                && now != 32 && now != 0 {
+            str += String(bytes: [now], encoding: .ascii)!
+            now = read()
+        }
+        
+        return str
+    }
+}
--- 37_최소 신장 트리/06_17472.swift
+++ 37_최소 신장 트리/06_17472.swift
@@ -1,1 +1,158 @@
+typealias Point = (x: Int, y: Int)
+typealias Edge = (u: Int, v: Int, dist: Int)
 
+func isValidCoord(_ point: Point) -> Bool {
+    return 0..<m ~= point.x && 0..<n ~= point.y
+}
+
+func setNumber(_ start: Point, _ number: Int) {
+    var queue = [start]
+    var index = 0
+
+    map[start.y][start.x] = number
+    visited[start.y][start.x] = true
+
+    while index < queue.count {
+        let current = queue[index]
+        index += 1
+
+        for i in 0..<4 {
+            let next = Point(current.x + dx[i], current.y + dy[i])
+
+            if isValidCoord(next), !visited[next.y][next.x], map[next.y][next.x] == 1 {
+                visited[next.y][next.x] = true
+                map[next.y][next.x] = number
+                queue.append(next)
+            }
+        }
+    }
+}
+
+// 현재 좌표에서 상하좌우 확인 후, 바다인 방향 인덱스 리턴
+// 0, 1, 2, 3 (상, 하, 좌, 우)
+func getBridgeDirections(_ point: Point) -> [Int] {
+    var directions: [Int] = []
+    for i in 0..<4 {
+        let next = Point(point.x + dx[i], point.y + dy[i])
+        if !isValidCoord(next) { continue }
+        if map[next.y][next.x] == 0 {
+            directions.append(i)
+        }
+    }
+    return directions
+}
+
+// 해당 좌표에서 다리 놓기
+// - Parameters:
+//   - point: 출발 섬의 좌표 정보
+//   - number: 출발 섬 번호
+//   - direction: 다리를 놓는 방향
+//   - dist: 다리의 길이
+func makeBridge(_ point: Point, _ number: Int, _ direction: Int, _ dist: Int) {
+    var queue = [(point, direction, dist)]
+    var index = 0
+    
+    while index < queue.count {
+        let current = queue[index].0
+        let direction = queue[index].1
+        let dist = queue[index].2
+        
+        if map[current.y][current.x] == number && dist > 1 { break }
+        
+        if map[current.y][current.x] > 0 && map[current.y][current.x] != number {
+            if dist > 2 {
+                edges.append((number, map[current.y][current.x], dist - 1))
+            }
+            break
+        }
+        
+        let next = Point(current.x + dx[direction], current.y + dy[direction])
+        
+        if !isValidCoord(next) { break }
+        
+        queue.append((next, direction, dist + 1))
+        index += 1
+    }
+}
+
+func find(_ u: Int) -> Int {
+    if parent[u] == u { return u }
+    parent[u] = find(parent[u])
+    return parent[u]
+}
+
+func merge(_ u: Int, _ v: Int) {
+    let parentU = find(u)
+    let parentV = find(v)
+
+    if parentU < parentV {
+        parent[parentV] = parentU
+    } else if parentV < parentU {
+        parent[parentU] = parentV
+    }
+}
+
+func isCycle(_ u: Int, _ v: Int) -> Bool {
+    return find(u) == find(v)
+}
+
+func kruskal() -> Int {
+    var answer = 0
+    var edgesCount = 0
+    
+    edges.sort { $0.dist < $1.dist }
+
+    for (u, v, dist) in edges where !isCycle(u, v) {
+        merge(u, v)
+        answer += dist
+        edgesCount += 1
+    }
+
+    return edgesCount != number - 1 ? -1 : answer
+}
+
+let nm = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+let (n, m) = (nm[0], nm[1])
+
+var map = [[Int]](repeating: [], count: n)
+
+for i in 0..<n {
+    let input = (readLine() ?? "").split { $0 == " " }.map { Int($0) ?? 0 }
+    map[i] = input
+}
+
+// 1. 섬에 번호 부여
+var visited = [[Bool]](repeating: [Bool](repeating: false, count: m), count: n)
+var number = 0
+let dx = [0, 0, -1, 1]
+let dy = [1, -1, 0, 0]
+
+for y in 0..<n {
+    for x in 0..<m {
+        if !visited[y][x], map[y][x] == 1 {
+            number += 1
+            setNumber(Point(x, y), number)
+        }
+    }
+}
+
+// 2. 섬 사이에 놓일 수 있는 다리(간선) 구하기
+var edges: [Edge] = []
+
+for y in 0..<n {
+    for x in 0..<m {
+        if map[y][x] > 0 {
+            let point = Point(x, y)
+            // 바다와 인접한 방향
+            let direction = getBridgeDirections(point)
+            direction.forEach {
+                makeBridge(point, map[y][x], $0, 0)
+            }
+        }
+    }
+}
+
+// 3. 최단 거리 간선의 합 구하기
+var parent = [Int](0...number)
+
+print(kruskal())
